e0006be32590a1833bd53c9b3afe7b2865030995
aqulzz <am.fadwa15@gmail.com>
2021-09-20 22:26:01 +0700

    Update Uploader

---
+++ app.js
@@ -0,0 +1,87 @@
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import multer from 'multer';
+import path from 'path';
+
+const app = express()
+const port = 8000
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
+app.set('json spaces', 2)
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(cookieParser())
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+const storage = multer.diskStorage({
+    destination: 'public/file',
+    filename: (req, file, cb) => {
+        cb(null, makeid(12) +
+            path.extname(file.originalname))
+    }
+});
+
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 10000000 // 10 MB
+    }
+})
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile('./public/index.html')
+})
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    if (!req.file.path) return res.status(400).json({
+        status: false,
+        message: "No file uploaded"
+    })
+    res.status(200).json({
+        status: true,
+        message: "Created by aqulzz",
+        result: {
+            originalname: req.file.originalname,
+            encoding: req.file.encoding,
+            mimetype: req.file.mimetype,
+            size: req.file.size,
+            url: "http://" + req.hostname + "/file/" + req.file.filename
+        }
+    })
+}, (error, req, res, next) => {
+    res.status(400).json({
+        error: error.message
+    })
+})
+
+// Handling 404
+app.use(function (req, res, next) {
+    res.status(404).json({
+        status: false,
+        message: "Page not found"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
